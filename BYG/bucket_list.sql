94127fe12033f0d8cd58ed59f7abde74dca70bd0
Judah Guggenheim <guggenheimjm@gmail.com>
2025-03-26 22:40:35 -0400

    tweaked DB

--- BYG/bucket_list.sql
+++ BYG/bucket_list.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE bucket_list (
     bucket_id SERIAL PRIMARY KEY,
-    item TEXT,
+    title TEXT,
     contact TEXT,
     area TEXT,
     descrip TEXT, 
@@ -22,10 +22,10 @@ CREATE TABLE users (
     bucket_id TEXT
 );
 
-INSERT INTO bucket_list (contact, area, descrip, category, cloudinary_id)
+INSERT INTO bucket_list (title, contact, area, descrip, category, cloudinary_id)
     VALUES ('Ceramics Studio', '903-328-1390', 'New College West', 'Make clay pots.', 'creative', 'XXX');
-INSERT INTO bucket_list (contact, area, descrip, category, cloudinary_id)
+INSERT INTO bucket_list (title, contact, area, descrip, category, cloudinary_id)
     VALUES ('Climbing Wall', '721-675-8932', 'Dillon Gym', 'Scale the rock wall and see how you fare.', 'athletic', 'XXX');
-INSERT INTO bucket_list (contact, area, descrip, category, cloudinary_id)
+INSERT INTO bucket_list (title, contact, area, descrip, category, cloudinary_id)
     VALUES ('Maker Space', '888-888-8888', 'Lewis Library', '3D-print and create what you can imagine.', 'creative', 'XXX');
 
