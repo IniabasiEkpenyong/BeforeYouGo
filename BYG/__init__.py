40c1810b7ab84012e3968f1917b6572229766ba8
Iniabasi Ekpenyong <iniek@Ini.(none)>
2025-04-03 06:32:15 -0400

    Added a feature for a global bucketlist item

--- BYG/__init__.py
+++ BYG/__init__.py
@@ -1,1 +1,9 @@
 # Makes BYG a module
+
+from flask import Flask
+
+app = Flask(__name__, template_folder='.')
+app.secret_key = 'development_key_123'  # You should change this to use environment variable
+
+from . import auth
+from . import byg
