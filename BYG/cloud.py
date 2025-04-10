dffc1e5d1f8b7a9237e961ac57aec0c3f6fd4fe8
Iniabasi Ekpenyong <iniek@Ini.(none)>
2025-04-10 06:35:17 -0400

    Some more styling

--- BYG/cloud.py
+++ BYG/cloud.py
@@ -1,3 +1,47 @@
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+import os
+from pathlib import Path
+import dotenv
+
+env_path = Path(__file__).resolve().parent / '.env'
+dotenv.load_dotenv(env_path)
+
+folder = './images'
+tfolder = 'BucketListPics'
+
+def main():
+
+    cloudinary.config(
+        cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
+        api_key = os.getenv("CLOUDINARY_API_KEY"),
+        api_secret = os.getenv("CLOUDINARY_API_SECRET"),
+        secure = True
+    )
+    for filename in os.listdir(folder):
+            f = filename.lower();
+            if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.avif') :
+                file_path = os.path.join(folder, filename)
+                public_id = os.path.splitext(filename)[0]
+                try:
+                    upload_result = cloudinary.uploader.upload(file_path, folder = tfolder,
+                                                public_id=public_id)
+                    print(upload_result['public_id'])
+                    retrieve_image(public_id)
+                except Exception as e:
+                    print (f"Failed to upload {filename} : {e}")
+
+def retrieve_image(public_id):
+    try:
+        response = cloudinary.api.resources(type='upload', prefix=tfolder, public_id=public_id)
+        print(response)
+    except Exception as e:
+        print(f"Failed to retrieve image: {e}")
+
+if __name__ == "__main__":
+    main()
+
 
 
 
