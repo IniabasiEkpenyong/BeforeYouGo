87b0c89113e0b71969cd8df5a4f53748c5d58077
Iniabasi Ekpenyong <iniek@Ini.(none)>
2025-04-10 08:41:58 -0400

    Fixing cloudinary stuff

--- BYG/cloud.py
+++ BYG/cloud.py
@@ -28,16 +28,41 @@ def main():
                     upload_result = cloudinary.uploader.upload(file_path, folder = tfolder,
                                                 public_id=public_id)
                     print(upload_result['public_id'])
-                    retrieve_image(public_id)
+                    print(retrieve_image(public_id))
                 except Exception as e:
                     print (f"Failed to upload {filename} : {e}")
 
 def retrieve_image(public_id):
     try:
-        response = cloudinary.api.resources(type='upload', prefix=tfolder, public_id=public_id)
-        print(response)
+        response = cloudinary.api.resource(f"{tfolder}/{public_id}")
+        return response.get('url')
     except Exception as e:
         print(f"Failed to retrieve image: {e}")
+
+def add_images_to_database():
+    cloudinary.config(
+        cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
+        api_key = os.getenv("CLOUDINARY_API_KEY"),
+        api_secret = os.getenv("CLOUDINARY_API_SECRET"),
+        secure = True
+    )
+
+    with sqlalchemy.orm.Session(database._engine) as session_db:
+        for filename in os.listdir(folder):
+                f = filename.lower();
+                if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.avif') :
+                    item = session_db.query(Bucket).filter(Bucket.cloudinary_id == public_id).first()
+                    if item is None:
+                        break;
+                    else:
+                        try:
+                            upload_result = cloudinary.uploader.upload(file_path, folder = tfolder,
+                                                        public_id=public_id)
+                            print(upload_result['public_id'])
+                        except Exception as e:
+                            print (f"Failed to upload {filename} : {e}")
+
+
 
 if __name__ == "__main__":
     main()
