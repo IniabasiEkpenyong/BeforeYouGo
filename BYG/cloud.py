004bab19e0bb79722bb1c71b59de2143f9966651
Itoro Ekpenyong <itorekpen@gmail.com>
2025-04-17 12:52:52 -0400

    cloudinary

--- BYG/cloud.py
+++ BYG/cloud.py
@@ -5,13 +5,17 @@ import os
 from pathlib import Path
 import dotenv
 
+
 env_path = Path(__file__).resolve().parent / '.env'
 dotenv.load_dotenv(env_path)
+
 
 folder = './images'
 tfolder = 'BucketListPics'
 
+
 def main():
+
 
     cloudinary.config(
         cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
@@ -20,7 +24,7 @@ def main():
         secure = True
     )
     for filename in os.listdir(folder):
-            f = filename.lower();
+            f = filename.lower()
             if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.avif') :
                 file_path = os.path.join(folder, filename)
                 public_id = os.path.splitext(filename)[0]
@@ -32,12 +36,14 @@ def main():
                 except Exception as e:
                     print (f"Failed to upload {filename} : {e}")
 
+
 def retrieve_image(public_id):
     try:
         response = cloudinary.api.resource(f"{tfolder}/{public_id}")
         return response.get('url')
     except Exception as e:
         print(f"Failed to retrieve image: {e}")
+
 
 def add_images_to_database():
     cloudinary.config(
@@ -47,13 +53,14 @@ def add_images_to_database():
         secure = True
     )
 
+
     with sqlalchemy.orm.Session(database._engine) as session_db:
         for filename in os.listdir(folder):
-                f = filename.lower();
+                f = filename.lower()
                 if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.avif') :
                     item = session_db.query(Bucket).filter(Bucket.cloudinary_id == public_id).first()
                     if item is None:
-                        break;
+                        break
                     else:
                         try:
                             upload_result = cloudinary.uploader.upload(file_path, folder = tfolder,
@@ -64,8 +71,15 @@ def add_images_to_database():
 
 
 
+
+
+
 if __name__ == "__main__":
     main()
+
+
+
+
 
 
 
