097829870f2dc8ee9eedb0bc72680b54cc04d81a
Iniabasi Ekpenyong <iniek@Ini.(none)>
2025-04-23 21:11:59 -0400

    Admin system kind of works

--- BYG/bucket_list.sql
+++ BYG/bucket_list.sql
@@ -12,7 +12,10 @@ CREATE TABLE bucket_list (
     descrip TEXT, 
     category TEXT,
     cloudinary_id TEXT,
-    priv BOOLEAN DEFAULT FALSE
+    priv BOOLEAN DEFAULT FALSE,
+    status VARCHAR DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_by VARCHAR NOT NULL
 );
 
 CREATE TABLE user_bucket (
@@ -34,51 +37,50 @@ CREATE INDEX idx_subtasks_user_bucket_id ON subtasks(user_bucket_id);
 
 
 -- We can improve this logic later so we can linearize adding items
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Ceramics Studio', '903-328-1390', 'New College West', 40.3422959650295, -74.65496351594201, 'Make clay pots.', 'creative', 'ceramic.jpg', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Ceramics Studio', '903-328-1390', 'New College West', 40.3422959650295, -74.65496351594201, 'Make clay pots.', 'creative', 'ceramic.jpg', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Climbing Wall', '721-675-8932', 'Princeton Stadium', 40.34580906239536, -74.65000148021971, 'Scale the rock wall and see how you fare.', 'athletic', 'climbing', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Climbing Wall', '721-675-8932', 'Princeton Stadium', 40.34580906239536, -74.65000148021971, 'Scale the rock wall and see how you fare.', 'athletic', 'climbing', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Maker Space', '888-888-8888', 'Lewis Library', 40.34661008276812, -74.65258909200425, '3D-print and create what you can imagine.', 'creative', '3dprinter', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Maker Space', '888-888-8888', 'Lewis Library', 40.34661008276812, -74.65258909200425, '3D-print and create what you can imagine.', 'creative', '3dprinter', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Princeton Tunnels', '609-555-1001', 'Campus', 40.34886232734069, -74.6593445312841, 'Explore the mysterious underground tunnels.', 'adventure', 'explore.jpg', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Princeton Tunnels', '609-555-1001', 'Campus', 40.34886232734069, -74.6593445312841, 'Explore the mysterious underground tunnels.', 'adventure', 'explore.jpg', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Graduate College Tower', '609-555-1002', 'Graduate College', 40.340862645767565, -74.66442614477758, 'Climb to the top for a great view.', 'historic', 'XXX', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Graduate College Tower', '609-555-1002', 'Graduate College', 40.340862645767565, -74.66442614477758, 'Climb to the top for a great view.', 'historic', 'XXX', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Rockefeller Tower', '609-555-1003', 'Rockefeller College', 40.34860783791797, -74.66206518525743, 'See campus from the Rocky tower.', 'historic', 'tower.jpg', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Rockefeller Tower', '609-555-1003', 'Rockefeller College', 40.34860783791797, -74.66206518525743, 'See campus from the Rocky tower.', 'historic', 'tower.jpg', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('PNI Monkey Lab', '609-555-1004', 'PNI', 40.343480083733574, -74.65249748035964, 'Tour the cognitive neuroscience facilities.', 'academic', 'XXX', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('PNI Monkey Lab', '609-555-1004', 'PNI', 40.343480083733574, -74.65249748035964, 'Tour the cognitive neuroscience facilities.', 'academic', 'XXX', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Roma Theater Movie Night', '609-555-1005', 'Rockefeller College', 40.3487999889968, -74.6621349226885, 'Go to a movie night at the Roma.', 'social', 'movie.png', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Roma Theater Movie Night', '609-555-1005', 'Rockefeller College', 40.3487999889968, -74.6621349226885, 'Go to a movie night at the Roma.', 'social', 'movie.png', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Princeton Battlefield Tour', '609-555-1006', 'Princeton Battlefield', 40.33102075322586, -74.67671911779128, 'Visit the Revolutionary War site.', 'historic', 'XXX', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Princeton Battlefield Tour', '609-555-1006', 'Princeton Battlefield', 40.33102075322586, -74.67671911779128, 'Visit the Revolutionary War site.', 'historic', 'XXX', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Princeton Airport Flight Class', '609-555-1007', 'Princeton Airport', 40.39757271507362, -74.66030562758498, 'Take an introductory flying lesson.', 'adventure', 'XXX', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Princeton Airport Flight Class', '609-555-1007', 'Princeton Airport', 40.39757271507362, -74.66030562758498, 'Take an introductory flying lesson.', 'adventure', 'XXX', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Danny DeVito Shrine', '609-555-1008', 'Frist Campus Center', 40.346796147653855, -74.65514601594192, 'Visit the shrine of Danny DeVito.', 'quirky', 'danny.avif', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Danny DeVito Shrine', '609-555-1008', 'Frist Campus Center', 40.346796147653855, -74.65514601594192, 'Visit the shrine of Danny DeVito.', 'quirky', 'danny.avif', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Grounds for Sculpture', '609-555-1010', 'Hamilton, NJ', 40.23695376032633, -74.71875739200733, 'Explore the outdoor sculpture museum.', 'artistic', 'XXX', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Grounds for Sculpture', '609-555-1010', 'Hamilton, NJ', 40.23695376032633, -74.71875739200733, 'Explore the outdoor sculpture museum.', 'artistic', 'XXX', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Amish Farmers Market', '609-555-1011', 'Trenton Area', 40.38777817707781, -74.60158420538576, 'Eat and shop at the Amish market.', 'food', 'XXX', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Amish Farmers Market', '609-555-1011', 'Trenton Area', 40.38777817707781, -74.60158420538576, 'Eat and shop at the Amish market.', 'food', 'XXX', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('PPPL', '609-555-1013', 'Plasma Physics Lab', 40.35074928704113, -74.6028651932533, 'Tour the Princeton Plasma Physics Lab.', 'academic', 'XXX', FALSE);
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('PPPL', '609-555-1013', 'Plasma Physics Lab', 40.35074928704113, -74.6028651932533, 'Tour the Princeton Plasma Physics Lab.', 'academic', 'XXX', FALSE, 'approved', 'admin');
 
-INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
-VALUES ('Poe field stargazing', '609-555-1014', 'Poe Field', 40.34364531909187, -74.6550066803598, 'Relax and stargaze under the open sky.', 'reflective', 'XXX', FALSE);
-
+INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv, status, created_by) 
+VALUES ('Poe field stargazing', '609-555-1014', 'Poe Field', 40.34364531909187, -74.6550066803598, 'Relax and stargaze under the open sky.', 'reflective', 'XXX', FALSE, 'approved', 'admin');
 /*
 INSERT INTO bucket_list (item, contact, area, lat, lng, descrip, category, cloudinary_id, priv) 
 VALUES ('Farminary', '609-555-1015', 'Princeton Theological Seminary', 40.35338378714052, -74.66249214546603, 'Visit the farm run by the seminary.', 'sustainable', 'XXX', FALSE);
